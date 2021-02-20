e5f2791ec2a0e9b585cbd3522574ce42dad326f1
nourhan.jardali@cadmusdatascience.com <^JMS|-#DVhc%>
2021-02-20 12:32:28 +0200

    News App

---
+++ lib/services/api_service.dart
@@ -0,0 +1,40 @@
+//Now let's make the HTTP request service
+//this class will allow us to make a simple get HTTP request
+//from the API and get the Articles and then return a list of Articles
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:star_lebanon/model/article_model.dart';
+
+class ApiService {
+  //let's add an Endpoint URL, you can check the website documentation
+  //and learn about the different Endpoint
+  //for this example I'm going to use a single endpoint
+
+  final endPointUrl =
+      "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c02ba310753e4ce898fcd87e5980ab1b";
+
+  //Now let's create the http request function
+  //but first let's import the http package
+
+  Future<List<Article>> getArticle() async {
+    Response res = await get(endPointUrl);
+
+    //first of all let's check that we got a 200 status code:
+    //this means that the request was a success
+    if (res.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(res.body);
+      List<dynamic> body = json['articles'];
+
+      //this line will allow us to get the different articles from the json file
+      //and putting them into a list
+      List<Article> articles =
+          body.map((dynamic item) => Article.fromJson(item)).toList();
+
+      return articles;
+    } else {
+      throw ("Can't get the Articles");
+    }
+  }
+}
